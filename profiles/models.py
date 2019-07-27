de5061938bd286a9edf9a41fa2e6fbabf9fb11f2
shaokat <shaokatcse@gmail.com>
2019-07-28 05:20:03 +0600

    Enable the Django admin for the user profile model

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,59 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.models import BaseUserManager
+
 
 # Create your models here.
+class userProfileManager(BaseUserManager):
+
+    def create_user(self, email, name, password=None):
+        if not email:
+            raise ValueError('User must have an email address.')
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, name=name)
+
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, name, password):
+        user = self.create_user(email, name, password)
+        user.is_superuser = True
+        user.is_staff = True
+
+        user.save(using=self._db)
+
+        return user
+
+
+class UserProfile(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(max_length=25, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = userProfileManager()
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["name"]
+
+    def get_full_name(self):
+        return self.name
+
+    def get_short_naem(self):
+        return self.name
+
+    def __str__(self):
+        return self.email
+
+
+class ProfileFeedItem(models.Model):
+    user_profile = models.ForeignKey('UserProfile', on_delete=models.CASCADE)
+    status_text = models.CharField(max_length=255)
+    created_on = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.status_text
