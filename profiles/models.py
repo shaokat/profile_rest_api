80925af35493ffbd0d16b44fb5bfb9df2a39c9c8
shaokat <shaokatcse@gmail.com>
2019-07-29 23:13:38 +0600

    setup database

--- profiles/models.py
+++ profiles/models.py
@@ -5,55 +5,49 @@ from django.contrib.auth.models import BaseUserManager
 
 
 # Create your models here.
-class userProfileManager(BaseUserManager):
 
-    def create_user(self, email, name, password=None):
-        if not email:
-            raise ValueError('User must have an email address.')
+class UserProfileManager(BaseUserManager):
+  """Helps Django work with our custom user model."""
+  def create_user(self,email,name,password=None):
+    """Creats a new user profile object"""
+    if not email:
+      raise ValueError('Users must have an email address')
 
-        email = self.normalize_email(email)
-        user = self.model(email=email, name=name)
+    email = self.normalize_email(email)
+    user = self.model(email=email,name=name)
+    user.set_password(password)
+    user.save(using=self._db)
+    return user
 
-        user.set_password(password)
-        user.save(using=self._db)
+  def create_superuser(self,email,name,password):
+    """Creates and saves an new superuser with given details"""
 
-        return user
+    user = self.create_user(email,name,password)
+    user.is_superuser = True
+    user.is_staff = True
+    user.save(using=self._db)
 
-    def create_superuser(self, email, name, password):
-        user = self.create_user(email, name, password)
-        user.is_superuser = True
-        user.is_staff = True
+class UserProfile(AbstractBaseUser,PermissionsMixin):
 
-        user.save(using=self._db)
+  """Respents a "user profiele" inside our system."""
 
-        return user
+  email = models.EmailField(max_length=255,unique=True)
+  name = models.CharField(max_length=255)
+  is_active = models.BooleanField(default=True)
+  is_staff = models.BooleanField(default=False)
 
+  objects = UserProfileManager()
+  USERNAME_FIELD = 'email'
+  REQUIRED_FIELDS = ['name']
 
-class UserProfile(AbstractBaseUser, PermissionsMixin):
-    email = models.EmailField(max_length=25, unique=True)
-    name = models.CharField(max_length=255)
-    is_active = models.BooleanField(default=True)
-    is_staff = models.BooleanField(default=False)
+  def get_full_name(self):
+    """Used to get a users full name"""
 
-    objects = userProfileManager()
+    return self.name
+  def get_short_name(self):
+    """Used to get a users short name."""
+    return self.name
 
-    USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = ["name"]
-
-    def get_full_name(self):
-        return self.name
-
-    def get_short_naem(self):
-        return self.name
-
-    def __str__(self):
-        return self.email
-
-
-class ProfileFeedItem(models.Model):
-    user_profile = models.ForeignKey('UserProfile', on_delete=models.CASCADE)
-    status_text = models.CharField(max_length=255)
-    created_on = models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        return self.status_text
+  def __str__(self):
+    """Django uses this when it needs to convert the objects to string"""
+    return self.email
