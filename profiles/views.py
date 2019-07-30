e52c3af4a67848d34885185d51a15d2e1265468c
shaokat <shaokatcse@gmail.com>
2019-07-30 17:33:03 +0600

    Added permisions and authentication to our UserprofileView

--- profiles/views.py
+++ profiles/views.py
@@ -6,6 +6,8 @@ from rest_framework.views import Response
 from rest_framework import status
 from . import serializers
 from . import models
+from . import permissions
+from rest_framework.authentication import TokenAuthentication
 
 # Create your views here.
 
@@ -86,3 +88,5 @@ class UserProfileViewSet(viewsets.ModelViewSet):
 
   serializer_class = serializers.UserProfileSerializer
   queryset = models.UserProfile.objects.all()
+  authentication_classes = (TokenAuthentication,)
+  permission_classes = (permissions.UpdateOwnProfile,)
